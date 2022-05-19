bb6035c1fa46310bf5da2536cc22e8b239895e18
Braian <braian.gonzales77@gmail.com>
2022-05-19 20:06:01 -0300

    crear side menú base

--- components/layouts/ShopLayout.tsx
+++ components/layouts/ShopLayout.tsx
@@ -1,7 +1,7 @@
 import { maxWidth } from '@mui/system'
 import Head from 'next/head'
 import { FC, ReactNode } from 'react'
-import { Navbar } from '../ui'
+import { Navbar, SideMenu } from '../ui'
 
 interface Props {
   children?: ReactNode | undefined
@@ -32,7 +32,7 @@ export const ShopLayout: FC<Props> = ({
         <Navbar />
       </nav>
 
-      {/* TODO: Sidebar */}
+      <SideMenu />
 
       <main
         style={{ margin: '80px auto', padding: '0px 30px', maxWidth: '1440px' }}
