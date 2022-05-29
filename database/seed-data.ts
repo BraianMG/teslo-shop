bfbd7000e7a9593e1a5aea73d428a47bd5138a00
Braian <braian.gonzales77@gmail.com>
2022-05-29 18:55:25 -0300

    agregar usuarios de prueba a nuestro seed

--- database/seed-data.ts
+++ database/seed-data.ts
@@ -1,3 +1,5 @@
+import bcrypt from 'bcryptjs'
+
 interface SeedProduct {
   description: string
   images: string[]
@@ -11,14 +13,36 @@ interface SeedProduct {
   gender: 'men' | 'women' | 'kid' | 'unisex'
 }
 
+interface SeedUser {
+  name: string
+  email: string
+  password: string
+  role: 'admin' | 'client'
+}
+
 type ValidSizes = 'XS' | 'S' | 'M' | 'L' | 'XL' | 'XXL' | 'XXXL'
 type ValidTypes = 'shirts' | 'pants' | 'hoodies' | 'hats'
 
 interface SeedData {
+  users: SeedUser[]
   products: SeedProduct[]
 }
 
 export const initialData: SeedData = {
+  users: [
+    {
+      name: 'Admin',
+      email: 'admin@admin.com',
+      password: bcrypt.hashSync('12345678'),
+      role: 'admin',
+    },
+    {
+      name: 'Braian Gonzales',
+      email: 'braian@braian.com',
+      password: bcrypt.hashSync('12345678'),
+      role: 'client',
+    },
+  ],
   products: [
     {
       description:
