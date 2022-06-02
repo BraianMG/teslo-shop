434fb0a17e20d7b464ee3015bb99612dba8accbf
Braian <braian.gonzales77@gmail.com>
2022-06-01 22:19:58 -0300

    aplicar validación con backend y mostrar mensajes de error en página de registro

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,53 +1,136 @@
+import { useState } from 'react'
 import NextLink from 'next/link'
-import { Box, Button, Grid, Link, TextField, Typography } from '@mui/material'
+import {
+  Box,
+  Button,
+  Chip,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+} from '@mui/material'
 import { AuthLayout } from '../../components/layouts'
+import { useForm } from 'react-hook-form'
+import { validations } from '../../utils'
+import { ErrorOutline } from '@mui/icons-material'
+import { tesloApi } from '../../api'
+
+type FormData = {
+  name: string
+  email: string
+  password: string
+}
 
 const RegisterPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>()
+  const [showError, setShowError] = useState(false)
+
+  const onRegisterForm = async ({ name, email, password }: FormData) => {
+    setShowError(false)
+    try {
+      const { data } = await tesloApi.post('/user/register', {
+        name,
+        email,
+        password,
+      })
+      const { token, user } = data
+      console.log({ token, user })
+    } catch (error) {
+      console.log('Error en las credenciales')
+      setShowError(true)
+      setTimeout(() => setShowError(false), 3000)
+    }
+
+    // TODO: navegar a la pantalla en la que estaba el usuario
+  }
+
   return (
     <AuthLayout title="Ingresar">
-      <Box sx={{ width: 350, padding: '10px 20px' }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Typography variant="h1" component="h1">
-              Crear cuenta
-            </Typography>
-          </Grid>
+      <form onSubmit={handleSubmit(onRegisterForm)} noValidate>
+        <Box sx={{ width: 350, padding: '10px 20px' }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography variant="h1" component="h1">
+                Crear cuenta
+                <Chip
+                  label="Correo ya registrado"
+                  color="error"
+                  icon={<ErrorOutline />}
+                  className="fadeIn"
+                  sx={{ display: showError ? 'flex' : 'none' }}
+                />
+              </Typography>
+            </Grid>
 
-          <Grid item xs={12}>
-            <TextField label="Nombre completo" variant="filled" fullWidth />
-          </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Nombre completo"
+                type="text"
+                variant="filled"
+                fullWidth
+                {...register('name', {
+                  required: 'Este campo es requerido',
+                  minLength: { value: 2, message: 'Mínimo 2 caracteres' },
+                })}
+                error={!!errors.name}
+                helperText={errors.name?.message}
+              />
+            </Grid>
 
-          <Grid item xs={12}>
-            <TextField label="Correo" variant="filled" fullWidth />
-          </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Correo"
+                type="email"
+                variant="filled"
+                fullWidth
+                {...register('email', {
+                  required: 'Este campo es requerido',
+                  validate: validations.isEmail,
+                })}
+                error={!!errors.email}
+                helperText={errors.email?.message}
+              />
+            </Grid>
 
-          <Grid item xs={12}>
-            <TextField
-              label="Contraseña"
-              type="password"
-              variant="filled"
-              fullWidth
-            />
-          </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Contraseña"
+                type="password"
+                variant="filled"
+                fullWidth
+                {...register('password', {
+                  required: 'Este campo es requerido',
+                  minLength: { value: 8, message: 'Mínimo 8 caracteres' },
+                })}
+                error={!!errors.password}
+                helperText={errors.password?.message}
+              />
+            </Grid>
 
-          <Grid item xs={12}>
-            <Button
-              color="secondary"
-              className="circular-btn"
-              size="large"
-              fullWidth
-            >
-              Registrarme
-            </Button>
-          </Grid>
+            <Grid item xs={12}>
+              <Button
+                type="submit"
+                color="secondary"
+                className="circular-btn"
+                size="large"
+                fullWidth
+              >
+                Registrarme
+              </Button>
+            </Grid>
 
-          <Grid item xs={12} display="flex" justifyContent="end">
-            <NextLink href="/auth/login" passHref>
-              <Link underline="always">¿Ya tienes cuenta?</Link>
-            </NextLink>
+            <Grid item xs={12} display="flex" justifyContent="end">
+              <NextLink href="/auth/login" passHref>
+                <Link underline="always">¿Ya tienes cuenta?</Link>
+              </NextLink>
+            </Grid>
           </Grid>
-        </Grid>
-      </Box>
+        </Box>
+      </form>
     </AuthLayout>
   )
 }
