4439967f672dfc214decde07e6402cd090de3885
Braian <braian.gonzales77@gmail.com>
2022-06-04 23:57:19 -0300

    utilizar AuthContext en página de login

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import NextLink from 'next/link'
 import {
   Box,
@@ -14,6 +14,8 @@ import { AuthLayout } from '../../components/layouts'
 import { validations } from '../../utils'
 import { tesloApi } from '../../api'
 import { ErrorOutline } from '@mui/icons-material'
+import { AuthContext } from '../../context'
+import { useRouter } from 'next/router'
 
 type FormData = {
   email: string
@@ -21,25 +23,27 @@ type FormData = {
 }
 
 const LoginPage = () => {
+  const router = useRouter()
+  const { logginUser } = useContext(AuthContext)
+  const [showError, setShowError] = useState(false)
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<FormData>()
-  const [showError, setShowError] = useState(false)
 
   const onLoginUser = async ({ email, password }: FormData) => {
     setShowError(false)
-    try {
-      const { data } = await tesloApi.post('/user/login', { email, password })
-      const { token, user } = data
-    } catch (error) {
-      console.log('Error en las credenciales')
+    const isValidLogin = await logginUser(email, password)
+
+    if (!isValidLogin) {
       setShowError(true)
       setTimeout(() => setShowError(false), 3000)
+      return
     }
 
     // TODO: navegar a la pantalla en la que estaba el usuario
+    router.replace('/')
   }
 
   return (
