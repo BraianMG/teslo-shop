61beb74e762d76acdcc4f6fd6458271a3851bc95
Braian <braian.gonzales77@gmail.com>
2022-07-23 22:15:13 -0300

    proveedor - credenciales

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,10 +1,37 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+import Credentials from 'next-auth/providers/credentials'
+
 export default NextAuth({
   providers: [
+    Credentials({
+      name: 'Custom Login',
+      credentials: {
+        email: {
+          label: 'Correo:',
+          type: 'email',
+          placeholder: 'correo@google.com',
+        },
+        password: {
+          label: 'Contraseña:',
+          type: 'password',
+          placeholder: 'Contraseña',
+        },
+      },
+      async authorize(credentials) {
+        console.log({ credentials })
+        // TODO: validar contra base de datos
+        return { name: 'Braian', correo: 'admin@admin.com', role: 'admin' }
+      },
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID || '',
       clientSecret: process.env.GITHUB_SECRET || '',
     }),
   ],
+
+  // Callbacks
+  callbacks: {
+    
+  }
 })
