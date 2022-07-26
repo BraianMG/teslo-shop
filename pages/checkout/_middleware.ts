6a72a28453b7bc5ffa1d91a5b2311e1e0bcfb25a
Braian <braian.gonzales77@gmail.com>
2022-07-26 20:58:30 -0300

    ajustar Middleware de checkout con NextAuth

--- pages/checkout/_middleware.ts
+++ pages/checkout/_middleware.ts
@@ -1,19 +1,26 @@
+import { getToken } from 'next-auth/jwt'
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
 // import { jwt } from '../../utils'
 
 // TODO: Este middleware no funciona correctamente por un problema de incompatibilidad entre Next.js y crypto
 export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+  const session = await getToken({req, secret: process.env.NEXTAUTH_SECRET})
+
+  console.log({session})
+
+  if (!session) {
+    const { origin } = req.nextUrl.clone()
+    const requestPage = req.page.name
+    return NextResponse.redirect(`${origin}/auth/login?p=${requestPage}`)
+  }
   return NextResponse.next()
 
   // const { token = '' } = req.cookies
 
   // try {
-  //   console.log('Antes del JWT')
   //   await jwt.isValidToken(token)
-  //   console.log('Después del JWT')
   //   return NextResponse.next()
   // } catch (error) {
-  //   console.log('En el catch')
   //   const requestPage = req.page.name
   //   return NextResponse.redirect(`/auth/login?p=${requestPage}`)
   // }
