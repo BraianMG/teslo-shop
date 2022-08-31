dab6b7b4a8ce5045a3209ce5fc9d21053443130a
Braian <braian.gonzales77@gmail.com>
2022-08-30 21:33:31 -0300

    mostrar todas las ordenes

--- pages/admin/orders.tsx
+++ pages/admin/orders.tsx
@@ -1,14 +1,79 @@
 import { ConfirmationNumberOutlined } from '@mui/icons-material'
+import { Chip, Grid } from '@mui/material'
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid'
 import React from 'react'
+import useSWR from 'swr'
 import { AdminLayout } from '../../components/layouts'
+import { IOrder, IUser } from '../../interfaces'
+
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'Orden ID', width: 250 },
+  { field: 'email', headerName: 'Correo', width: 250 },
+  { field: 'name', headerName: 'Nombre completo', width: 300 },
+  { field: 'total', headerName: 'Monto total', width: 250 },
+  {
+    field: 'isPaid',
+    headerName: 'Pagada',
+    renderCell: ({ row }: GridValueGetterParams) => {
+      return row.isPaid ? (
+        <Chip variant="outlined" label="Pagada" color="success" />
+      ) : (
+        <Chip variant="outlined" label="Pendiente" color="error" />
+      )
+    },
+  },
+  {
+    field: 'noProducts',
+    headerName: 'No. Productos',
+    align: 'center',
+    width: 150,
+  },
+  {
+    field: 'check',
+    headerName: 'Ver orden',
+    renderCell: ({ row }: GridValueGetterParams) => {
+      return (
+        <a href={`/admin/orders/${row.id}`} target="_blank" rel="noreferrer">
+          Ver orden
+        </a>
+      )
+    },
+  },
+  { field: 'createdAt', headerName: 'Creada el', width: 300 },
+]
 
 const OrdersPage = () => {
+  const { data, error } = useSWR<IOrder[]>('/api/admin/orders')
+
+  if (!data && !error) return <></>
+
+  const rows = data!.map((order) => ({
+    id: order._id,
+    email: (order.user as IUser).email,
+    name: (order.user as IUser).name,
+    total: order.orderSummary.total,
+    isPaid: order.isPaid,
+    noProducts: order.orderSummary.numberOfItems,
+    createdAt: order.createdAt,
+  }))
+
   return (
     <AdminLayout
       title="Ordenes"
       subTitle="Mantenimiento de ordenes"
       icon={<ConfirmationNumberOutlined />}
-    ></AdminLayout>
+    >
+      <Grid container className="fadeIn">
+        <Grid item xs={12} sx={{ height: 650, width: '100%' }}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={10}
+            rowsPerPageOptions={[10]}
+          />
+        </Grid>
+      </Grid>
+    </AdminLayout>
   )
 }
 
