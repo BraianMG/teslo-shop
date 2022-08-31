a8d5c62cc68e705403eabf8f698d3feded2888a3
Braian <braian.gonzales77@gmail.com>
2022-08-30 21:02:43 -0300

    cambiar rol de usuario

--- pages/admin/users.tsx
+++ pages/admin/users.tsx
@@ -1,23 +1,63 @@
 import { PeopleOutline } from '@mui/icons-material'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { AdminLayout } from '../../components/layouts'
-import { DataGrid, GridColDef } from '@mui/x-data-grid'
-import { Grid } from '@mui/material'
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid'
+import { Grid, MenuItem, Select } from '@mui/material'
 import useSWR from 'swr'
 import { IUser } from '../../interfaces'
+import { tesloApi } from '../../api'
 
 const UsersPage = () => {
   const { data, error } = useSWR<IUser[]>('/api/admin/users')
+  const [users, setUsers] = useState<IUser[]>([])
+
+  useEffect(() => {
+    if (data) setUsers(data)
+  }, [data])
 
   if (!data && !error) return <></>
+
+  const onRoleUpdated = async (userId: string, newRole: string) => {
+    const previousUsers = users.map((user) => ({ ...user }))
+    const updatedUsers = users.map((user) => ({
+      ...user,
+      role: userId === user._id ? newRole : user.role,
+    }))
+    setUsers(updatedUsers)
+    try {
+      await tesloApi.put('/admin/users', { userId, newRole })
+    } catch (error) {
+      setUsers(previousUsers)
+      console.log(error)
+      alert('No se pudo actualizar el role del usuario')
+    }
+  }
 
   const columns: GridColDef[] = [
     { field: 'email', headerName: 'Correo', width: 250 },
     { field: 'name', headerName: 'Nombre completo', width: 300 },
-    { field: 'role', headerName: 'Rol', width: 300 },
+    {
+      field: 'role',
+      headerName: 'Rol',
+      width: 300,
+      renderCell: ({ row }: GridValueGetterParams) => {
+        return (
+          <Select
+            value={row.role}
+            label="Rol"
+            onChange={({ target }) => onRoleUpdated(row.id, target.value)}
+            sx={{ width: '300px' }}
+          >
+            <MenuItem value="admin">Admin</MenuItem>
+            <MenuItem value="client">Client</MenuItem>
+            <MenuItem value="super-user">Super User</MenuItem>
+          </Select>
+        )
+      },
+    },
   ]
 
-  const rows = data!.map((user) => ({
+  const rows = users!.map((user) => ({
     id: user._id,
     email: user.email,
     name: user.name,
