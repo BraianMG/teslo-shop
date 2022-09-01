3574fbe66660ab605ef83603cb1ec8ec925056a8
Braian <braian.gonzales77@gmail.com>
2022-09-01 20:21:26 -0300

    crear endpoint para actualizar productos

--- pages/api/admin/products.ts
+++ pages/api/admin/products.ts
@@ -2,12 +2,14 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { IProduct } from '../../../interfaces'
 import { db } from '../../../database'
 import { Product } from '../../../models'
+import { isValidObjectId } from 'mongoose'
 
 type Data =
   | {
       message: string
     }
   | IProduct[]
+  | IProduct
 
 export default function handler(
   req: NextApiRequest,
@@ -18,7 +20,7 @@ export default function handler(
       return getProducts(req, res)
 
     case 'PUT':
-      return res.status(400).json({ message: 'Bad request' })
+      return updateProduct(req, res)
 
     case 'POST':
       return res.status(400).json({ message: 'Bad request' })
@@ -36,4 +38,39 @@ const getProducts = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   // TODO: Tendremos que actualizar las imágenes
 
   return res.status(400).json(products)
+}
+const updateProduct = async (
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) => {
+  const { _id = '', images = [] } = req.body as IProduct
+  if (!isValidObjectId(_id))
+    return res.status(400).json({ message: 'El ID del producto no es válido' })
+
+  if (images.length < 2)
+    return res.status(400).json({ message: 'Es necesario al menos 2 imágenes' })
+
+  // TODO: posiblemente tendremos un localhost:3000/products/asdasd.jpg
+
+  try {
+    await db.connect()
+    const product = await Product.findById(_id)
+    if (!product) {
+      await db.disconnect()
+      return res
+        .status(400)
+        .json({ message: 'No existe un producto con ese ID' })
+    }
+
+    // TODO: eliminar/agregar imágenes en Cloudinary
+
+    await product.update(req.body)
+    await db.disconnect()
+
+    return res.status(200).json(product)
+  } catch (error) {
+    console.log(error)
+    await db.disconnect()
+    return res.status(400).json({ message: 'Revisar la consola del servidor' })
+  }
 }
